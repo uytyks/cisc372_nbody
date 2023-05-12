f0f6ab6c04929ae112ee290fb81c2c45e7a6c4a1
Zachary England <90047997+uytyks@users.noreply.github.com>
2023-05-12 13:33:50 -0400

    progress

--- compute.c
+++ compute.c
@@ -11,58 +11,71 @@
 
 
 __global__ void arraySet(vector3* v, vector3** a){
-	int in = threadIdx.x;
-	for (i=in;i<NUMENTITIES;in += blockDim.x)
+        int in = threadIdx.x;
+        for (i=in;i<NUMENTITIES;in += blockDim.x)
                 a[i]=&v[i*NUMENTITIES];
 }
 
+__global__ void accelComp(){
+        int in = threadIdx.x;
+        for (i=0;i<NUMENTITIES;i++){
+                for (j=0;j<NUMENTITIES;j++){
+                        if (i==j) {
+                                FILL_VECTOR(accels[i][j],0,0,0);
+                        }
+                        else{
+                                vector3 distance;
+                                for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
+                                double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
+                                double magnitude=sqrt(magnitude_sq);
+                                double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
+                                FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
+                        }
+                }
+        }
+}
 
+__global__ void sumMatrix(){
+        for (i=0;i<NUMENTITIES;i++){
+                vector3 accel_sum={0,0,0};
+                for (j=0;j<NUMENTITIES;j++){
+                        for (k=0;k<3;k++)
+                                accel_sum[k]+=accels[i][j][k];
+                }
+                //compute the new velocity based on the acceleration and time interval
+                //compute the new position based on the velocity and time interval
+                for (k=0;k<3;k++){
+                        hVel[i][k]+=accel_sum[k]*INTERVAL;
+                        hPos[i][k]=hVel[i][k]*INTERVAL;
+                }
+        }
+}
 void compute(){
-	//make an acceleration matrix which is NUMENTITIES squared in size;
-	int i,j,k;
-	vector3* values;
-	vector3** accels;
-	//device memory start
-	//vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
-	//vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
-	cudaMalloc(&values,sizeof(vector3*)*NUMENTITIES*NUMENTITIES);
-	cudaMalloc(&accels,sizeof(vector3**)*NUMENTITIES);
-	//for (i=0;i<NUMENTITIES;i++)
-	//	accels[i]=&values[i*NUMENTITIES];
-	arraySet<<<1,100>>>(values,accels);`
-	//first compute the pairwise accelerations.  Effect is on the first argument.
-	for (i=0;i<NUMENTITIES;i++){
-		for (j=0;j<NUMENTITIES;j++){
-			if (i==j) {
-				FILL_VECTOR(accels[i][j],0,0,0);
-			}
-			else{
-				vector3 distance;
-				for (k=0;k<3;k++) distance[k]=hPos[i][k]-hPos[j][k];
-				double magnitude_sq=distance[0]*distance[0]+distance[1]*distance[1]+distance[2]*distance[2];
-				double magnitude=sqrt(magnitude_sq);
-				double accelmag=-1*GRAV_CONSTANT*mass[j]/magnitude_sq;
-				FILL_VECTOR(accels[i][j],accelmag*distance[0]/magnitude,accelmag*distance[1]/magnitude,accelmag*distance[2]/magnitude);
-			}
-		}
-	}
-	//sum up the rows of our matrix to get effect on each entity, then update velocity and position.
-	for (i=0;i<NUMENTITIES;i++){
-		vector3 accel_sum={0,0,0};
-		for (j=0;j<NUMENTITIES;j++){
-			for (k=0;k<3;k++)
-				accel_sum[k]+=accels[i][j][k];
-		}
-		//compute the new velocity based on the acceleration and time interval
-		//compute the new position based on the velocity and time interval
-		for (k=0;k<3;k++){
-			hVel[i][k]+=accel_sum[k]*INTERVAL;
-			hPos[i][k]=hVel[i][k]*INTERVAL;
-		}
-	}
-	//free(accels);
-	//free(values);
-	//free device memory
-	cudaFree(accels);
-	cudaFree(values);
+        //make an acceleration matrix which is NUMENTITIES squared in size;
+        int i,j,k;
+        vector3* values;
+        vector3** accels;
+        double* d_mass;
+        //device memory start
+        //vector3* values=(vector3*)malloc(sizeof(vector3)*NUMENTITIES*NUMENTITIES);
+        //vector3** accels=(vector3**)malloc(sizeof(vector3*)*NUMENTITIES);
+        cudaMalloc(&values,sizeof(vector3*)*NUMENTITIES*NUMENTITIES);
+        cudaMalloc(&accels,sizeof(vector3**)*NUMENTITIES);
+        cudaMalloc(&d_mass,sizeof(float*));
+
+        cudaMemcpy(d_hPos,hPos,sizeof(hPos),cudaMemcpyHostToDevice);
+        cudaMemcpy(d_hVel,hVel,sizeof(hVel),cudaMemcpyHostToDevice);
+        cudaMemcpy(d_mass,mass,sizeof(mass),cudaMemcpyHostToDevice);
+        //for (i=0;i<NUMENTITIES;i++)
+        //      accels[i]=&values[i*NUMENTITIES];
+        arraySet<<<1,100>>>(values,accels);`
+        //first compute the pairwise accelerations.  Effect is on the first argument.
+        accelComp<<<1,100>>>();
+        //sum up the rows of our matrix to get effect on each entity, then update velocity and position.
+        sumMatrix<<<1,100>>>();
+        //free(accels);
+        //free(values);
+        //free device memory
+        cudaFree(accels);
+        cudaFree(values);
 }
